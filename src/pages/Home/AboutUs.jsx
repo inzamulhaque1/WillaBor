1b0d99c33707c18156e71ece10f307dc0d85a2fe
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2025-05-12 23:09:45 +0600

    added about us

--- src/pages/Home/AboutUs.jsx
+++ src/pages/Home/AboutUs.jsx
@@ -1,11 +1,36 @@
-
+import logo from "../../assets/logo/logob.png";
+import ukryta from "../../assets/icons/ukryta.png";
+import tree from "../../assets/icons/tree.png";
+import log from "../../assets/icons/b1.png";
+import b from "../../assets/icons/b.png";
 
 const AboutUs = () => {
-    return (
-        <div>
-            About
+  return (
+    <div className="w-11/12 sm:w-8/12 mx-auto mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 justify-center items-center">
+        <div className="">
+          <div className="flex gap-2 sm:gap-4 h-8 sm:h-10 m-1 sm:m-2">
+            <img src={logo} alt="Logo" className="h-full" />
+            <img src={ukryta} alt="Ukryta" className="h-full" />
+          </div>
+          <p className="w-full sm:w-9/12 text-sm sm:text-base md:text-lg">
+            Choć znajduje się w samym sercu Zakopanego, Willa Bór zapewnia
+            wyjątkową ciszę i prywatność. Otoczona zielenią, daje wytchnienie po
+            dniu pełnym wrażeń. Idealne miejsce na wypoczynek – blisko Krupówek,
+            górskich szlaków i najlepszych restauracji, ale bez tłumów i hałasu.
+          </p>
         </div>
-    );
+
+        <div className="md:flex p-4 sm:p-6 md:p-8 lg:p-10 relative">
+          <img className="-mr-20 sm:" src={tree} alt="Tree" />
+          <div className="flex w-44 md:w-fit">
+            <img className="mr-2 sm:mr-5" src={log} alt="Log" />
+            <img src={b} alt="B" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -6,6 +6,7 @@ import {
   FaPlayCircle,
 } from "react-icons/fa";
 import play from "../../assets/icons/play1.png";
+
 const Banner = () => {
   return (
     <div
@@ -14,17 +15,21 @@ const Banner = () => {
     >
       {/* Centered Text */}
       <p
-        className="absolute text-white text-2xl sm:text-3xl md:text-6xl  text-center px-4 "
+        className="absolute text-white text-2xl sm:text-3xl md:text-6xl text-center px-4"
         style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
       >
-        W centrum Zakopanego <br />{" "}
-        <span className="font-rouge font-bold ">ukryta</span> wśród drzew
+        W centrum Zakopanego <br />
+        <span className="font-rouge font-bold">ukryta</span> wśród drzew
       </p>
 
-      <img className="h-36 " src={play} alt="" />
+      <img
+        className="h-20 sm:h-28 md:h-36 absolute top-5 left-5 sm:static"
+        src={play}
+        alt=""
+      />
 
       {/* Date Inputs and Button */}
-      <div className="absolute bottom-5 flex flex-col sm:flex-row items-center gap-3 sm:gap-4 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-5 flex flex-col sm:flex-row items-center gap-3 sm:gap-4 left-1/2 transform -translate-x-1/2 w-full px-4 sm:w-auto">
         <div className="relative">
           <div className="flex items-center border border-white rounded-lg overflow-hidden">
             <FaCalendarAlt className="text-white ml-2" />
@@ -44,21 +49,22 @@ const Banner = () => {
               className="text-white bg-transparent px-2 py-2 placeholder-white focus:outline-none w-36 sm:w-44"
             />
           </div>
-        </div>{" "}
-        <FaArrowRight className="text-white" />
-        <button className="bg-white text-black px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-gray-200 transition-colors">
+        </div>
+        <FaArrowRight className="text-white hidden sm:inline-block" />
+        <button className="bg-white text-black px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-gray-200 transition-colors w-fit">
           Rezerwuj
         </button>
       </div>
+
       {/* Google Rating */}
-      <div className="absolute bottom-10 right-20 flex items-center gap-1 text-white">
-        <span className="text-sm">G</span>
+      <div className="absolute bottom-24 sm:bottom-10 right-5 sm:right-20 flex items-center gap-1 text-white text-xs sm:text-sm">
+        <span className="">G</span>
         <div className="flex">
           {[...Array(5)].map((_, i) => (
             <FaStar key={i} className="text-white" />
           ))}
         </div>
-        <span className="text-sm">4,6 (674 opinii)</span>
+        <span className="">4,6 (674 opinii)</span>
       </div>
     </div>
   );
